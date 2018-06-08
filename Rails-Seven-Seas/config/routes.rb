daa2607d13c8d275f180c05d33e1002c409ab316
github username <github email address>
2018-06-08 15:35:17 -0400

    Done.

--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+resources :seas
+# ,only: [:new,:index,:create,:show,:edit,:update,:destroy]
+
+get "/",to: "seas#welcome"
 end
