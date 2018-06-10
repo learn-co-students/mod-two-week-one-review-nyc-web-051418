e3fe29154da4b5afada8ab43341e6ed868d70671
github username <github email address>
2018-06-10 11:21:14 -0400

    Done.

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,6 +1,18 @@
 class SeasController < ApplicationController
   #define your controller actions here
+  def index
+    @seas = Sea.all
+  end
 
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+  
+  
 
   private
   # In controller actions, use this private method to access sea params from forms.
--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Welcome Page
+  get '/' => 'static#index'
+  
+  # Seas Page
+  get '/seas' => 'seas#index'
+
+  # Show ID Pages
+  get '/seas/:id' => 'seas#show'
+
+  # Show ID Edit Page
+  get '/seas/:id/edit' => 'seas#edit'
 
 end
